a0f30ce02a312d9ce911a51f5cf7bd0087548eac
Your Name <you@example.com>
2024-03-27 20:13:46 -0300

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "",
+      }
+    },
+  },
+  plugins: [],
+  mode: "jit",
+}
+
